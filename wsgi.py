6967b59dcac7db12836af26069e409e80ff893e8
刘志曦 <liudiwu@gmail.com>
2014-10-15 23:33:11 +0800

    --

--- wsgi.py
+++ wsgi.py
@@ -6,11 +6,11 @@ def application(environ, start_response):
 	status = '200 OK'
 	response_headers = [('Content-type','text/html')]
 	start_response(status, response_headers)
-	path=environ['PATH_INFO']
+	path='static'+environ['PATH_INFO']
 	if isfile(path):
-		with open('static'+path,'r') as file:
+		with open(path,'r') as file:
 			return [file.read()]
 	if isdir(path):
-		dir=os.listdir('static'+path)
+		dir=os.listdir(path)
 		return ['<html><body>','\r\n'.join(['<a href="./%s" traget=_blank>%s</a><br/>'%(d,d) for d in dir]),'</body></html>']
 	return ['not find']
