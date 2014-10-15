62a6a1e7f1113ab9a6c22439c9c610f5b0288ff5
刘志曦 <liudiwu@gmail.com>
2014-10-15 23:46:27 +0800

    --

--- wsgi.py
+++ wsgi.py
@@ -4,15 +4,18 @@ import os
 
 def application(environ, start_response):
 	status = '200 OK'
-	response_headers = [('Content-type','text/html')]
-	start_response(status, response_headers)
 	path='static'+environ['PATH_INFO']
 	if path=='static/':
 		path='static/index.html'
+	if path[-3:]=='css':
+		response_headers = [('Content-type','text/css')]
+	else:
+		response_headers = [('Content-type','text/html')]
+	start_response(status, response_headers)
 	if isfile(path):
 		with open(path,'r') as file:
 			return [file.read()]
 	if isdir(path):
 		dir=os.listdir(path)
 		return ['<html><body>','\r\n'.join(['<a href="./%s" traget=_blank>%s</a><br/>'%(d,d) for d in dir]),'</body></html>']
-	return ['not find']
+	return ['<h1>not find</h1>']
