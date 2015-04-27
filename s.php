8654851afcb54ded18602fc5c92a6b6e7f61a2a1
Dream_ease <wurongjie1@139.com>
2015-04-27 18:49:34 +0800

    修复搜索bug

--- s.php
+++ s.php
@@ -1,13 +1,13 @@
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" /> 
 <?php
 if($_GET['s']!=""){
-	echo '<a href="index.html"><h2>返回首页</h2></a>';
+	echo '<a href="main.html"><h2>返回首页</h2></a>';
 	echo "搜索(",$_GET['s'],')结果如下:</br>';
 	foreach (glob("pkg/*.htm") as $name){
 		$s=file_get_contents($name);
 		$pos=strpos($s,$_GET['s']);
 		if ($pos!=false){
-			echo '<a href="',$name,'"><h2>',$name,'</h2></a> ...',substr($s,$pos-60,60*3),'...</br>';
+			echo '<a href="',$name,'"><h2>',$name,'</h2></a> ...',substr($s,$pos-60,90),"<strong>",$_GET['s'],"</strong>",substr($s,$pos,90),...</br>';
 	}
 	}
 }
